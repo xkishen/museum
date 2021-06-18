1336abec4b104548b66e522f1a68f912e6740f0f
xkishen <82388393+xkishen@users.noreply.github.com>
2021-06-18 08:25:54 -0700

    Update Main.java

--- Main/src/main/Main.java
+++ Main/src/main/Main.java
@@ -1,20 +1,25 @@
+
 package main;
+
+import java.awt.FontFormatException;
+import java.io.IOException;
 
 public class Main {
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException, FontFormatException, IOException {
+        
+        Frame frame = new Frame();
         WaitingLine waitline = new WaitingLine();
         Museum museum = new Museum();
-        Time time = new Time(museum);
+        Time time = new Time(museum, frame);
         TicketCounter ticketCounter = new TicketCounter(time, waitline);
-        Enter enter1 = new Enter(museum, time, waitline);
+        Enter enter1 = new Enter(museum, time, waitline, frame);
         Visitor v1 = new Visitor(ticketCounter, museum, time, waitline, enter1);
-        
+
         Thread t1 = new Thread(v1);
         Thread timer = new Thread(time);
         t1.start();
         timer.start();
-        
     }
     
 }
