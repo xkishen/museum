fc7e3e7430505b7d64cf54aaddf8aa6c76be4dc0
xkishen <82388393+xkishen@users.noreply.github.com>
2021-06-18 08:20:18 -0700

    Update Time.java

--- Main/src/main/Time.java
+++ Main/src/main/Time.java
@@ -1,19 +1,26 @@
+
 package main;
+
+import java.awt.Color;
+import java.text.DecimalFormat;
 
 public class Time implements Runnable{
     
     Museum museum;
     int min;
     int time;
+    Frame frame;
     
-    public Time(Museum museum){
+    public Time(Museum museum, Frame frame){
         this.museum = museum;
         this.min=480;
         this.time=0;
+        this.frame = frame;
     }
 
     @Override
     public void run() {
+        DecimalFormat df = new DecimalFormat("0000");
         for (int j = min; j < 1080; j++) {
             min++;
             time= ((min/60)*100)+(min%60);
@@ -23,7 +30,10 @@ public class Time implements Runnable{
             }
             if (min>=540) {
                 museum.open();
+                frame.setStatus("Open", new Color(231, 230, 247));
             }
+            frame.setClock(df.format(time));
         }
+        frame.setStatus("Close", new Color(132, 0, 50));
     }
 }
